2d9f575745d0e32c83786bb82f3d1ab17a8063f1
yashchinchole <iamyash337@gmail.com>
2022-10-18 01:27:00 +0530

    main

---
+++ 1_If_Else.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+public:
+    string compareNM(int n, int m)
+    {
+        if (n > m)
+            cout << "greater";
+        if (n == m)
+            cout << "equal";
+        if (n < m)
+            cout << "lesser";
+
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        Solution obj;
+        cout << obj.compareNM(n, m) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
