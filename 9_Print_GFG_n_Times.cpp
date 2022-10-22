6419a27525a66550863bca0cae0b012d061cf8fb
yashchinchole <iamyash337@gmail.com>
2022-10-23 02:10:22 +0530

    main

---
+++ 9_Print_GFG_n_Times.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    void printGfg(int n)
+    {
+        // Code here
+        if (n < 1)
+            return;
+        else
+        {
+            printGfg(n - 1);
+            cout << "GFG ";
+        }
+    }
+};
+
+//{ Driver Code Starts.
+/* Driver program to test printNos */
+int main()
+{
+    int T;
+
+    // taking testcases
+    cin >> T;
+
+    while (T--)
+    {
+        int N;
+
+        // input N
+        cin >> N;
+        Solution ob;
+        // calling printNos() function
+        ob.printGfg(N);
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
